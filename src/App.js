3a9183fdebc068830665a55843c741404621b658
Lucas Costa <luchtml5@gmail.com>
2022-10-27 18:42:33 -0300

    aprenrendo sobre estados

--- src/App.js
+++ src/App.js
@@ -1,46 +1,14 @@
-import React from "react"
+import React, { Component } from "react";
+import Membro from "./components/Membro";
+class App extends Component {
 
-const Equipe = (props) => {
-    return (
-        <div>
-            <Sobre nome={props.nome} cargo={props.cargo} idade={props.idade} />
-            <Social fb={props.facebook} />
-        </div>
-    )
-}
-
-const Sobre = (props) => {
-    return (
-        <div>
-            <h2>Olá sou o(a) {props.nome}</h2>
-            <h3>Cargo: {props.cargo}</h3>
-            <h3>Idade: {props.idade} anos</h3>
-            <Social />
-            <hr></hr>
-        </div>
-    )
-}
-
-const Social = (props) => {
-    return (
-        <div>
-            <a href={props.fb}>Linkedin </a>
-            <a> Github </a>
-            <a> Instagram</a>
-        </div>
-    )
-}
-
-function App(){
-    return(
-        <div>
-            <h1>Conheça nossa equipe:</h1>
-            <Equipe nome='Lucas' cargo='Programador' idade='20' facebook='https://google.com'/>
-            <Equipe nome='Thamires' cargo='Vendedora' idade='26'/>
-            <Equipe nome='Joana ela/dela' cargo='Dona de casa' idade='5'/>
-            <Equipe nome='E-lis-angela' cargo='Dona de tudo' idade='10'/>
-        </div>
-    )
+    render() {
+        return(
+            <div>
+                <Membro nome="Visitante"/>
+            </div>
+        )
+    }
 }
 
 export default App
